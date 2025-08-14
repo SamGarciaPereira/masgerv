b9223e9c61250a1a4aa8df0ef23b405b42af1ee8
Samuel Pereira <samgarciapereira@gmail.com>
2025-08-14 18:09:26 -0300

    sidebar redirecionando para as rotas

--- osMagserv/resources/views/layouts/main.blade.php
+++ osMagserv/resources/views/layouts/main.blade.php
@@ -4,14 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-white-600  font-inter">
+<body class="bg-white font-inter">
       <span id="open-sidebar-btn" class="absolute text-white text-4xl top-5 left-4 cursor-pointer">
     <i class="bi bi-filter-left px-2 bg-gray-900 rounded-md"></i>
   </span>
@@ -32,22 +31,22 @@
           <input class="text-[15px] ml-4 w-full bg-transparent focus:outline-none" placeholder="Pesquisar"/>
         </div>
 
-        <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
+        <a href="{{ route('home') }}" class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
           <i class="bi bi-house-door-fill"></i>
           <span class="text-[15px] ml-4 text-gray-200">Menu</span>
-        </div>
-        <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
+        </a>
+        <a href="{{ route('processo') }}"  class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
           <i class="bi bi-house-door-fill"></i>
           <span class="text-[15px] ml-4 text-gray-200">Processos</span>
-        </div>
-        <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
-          <i class="bi bi-bookmark-fill"></i>
+        </a>
+        <a href="{{ route('orcamentos') }}" class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
+          <i class="bi bi-clipboard2-fill"></i>
           <span class="text-[15px] ml-4 text-gray-200">Orçamentos</span>
-        </div>
-        <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
+        </a>
+        <a href="{{ route('manutencao') }}" class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
           <i class="bi bi-envelope-fill"></i>
           <span class="text-[15px] ml-4 text-gray-200">Manutenção</span>
-        </div>
+        </a>
         <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
           <i class="bi bi-piggy-bank-fill"></i>
           <div id="dropdown-btn" class="flex justify-between w-full items-center">
@@ -58,8 +57,8 @@
           </div>
         </div>
         <div class=" leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto" id="submenu">
-          <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Contas a pagar</h1>
-          <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Contas a receber</h1>
+          <a href="{{ route('financeiro.contas-pagar') }}" class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Contas a pagar</a>
+          <a href="{{ route('financeiro.contas-receber') }}" class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">Contas a receber</a>
         </div>
         <div class="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
           <i class="bi bi-box-arrow-in-right"></i>
@@ -74,4 +73,4 @@
     <p>Magserv Manutenção e Serviços ltda. &copy; 2025 | Samuel Software Developer </p>
 </footer>
 </body>
-</html>
+</html> 
